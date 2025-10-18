ae291d09281a0984e912a3bb27423b5345f8c59d
glubbfreund <oliver.leis@gmail.com>
2025-10-18 16:59:38 +0200

    updated scripts

--- .local/share/light-mode.d/other-apps.sh
+++ .local/share/light-mode.d/other-apps.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+
 cp ~/.config/OpenRGB/OpenRGB.json.light ~/.config/OpenRGB/OpenRGB.json
 sudo /home/oli/Dokumente/Src/spotifyIconPatch/icons_light/install-spotify-light.sh
 cp /home/oli/Dokumente/Src/steamIconPatch/light.png /home/oli/.steam/debian-installation/deb-installer/steam-launcher/icons/48/steam_tray_mono.png
 cp /home/oli/.config/fastfetch/config-light.jsonc /home/oli/.config/fastfetch/config.jsonc
 sudo /home/oli/Dokumente/Src/WasistlosIconPatch/install-light.sh
+
 sudo gtk-update-icon-cache -f /usr/share/icons/hicolor
+pkill xfce4-panel ; xfce4-panel &
